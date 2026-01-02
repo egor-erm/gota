346a374291a4c36e6906fca7b36b030a6beebf53
Ermolay <es.ermolaev2005@gmail.com>
2026-01-02 13:56:34 +0300

    Совместимость свечей с форматом json.

--- candle.go
+++ candle.go
@@ -6,45 +6,45 @@ import (
 
 // Candle - структура для хранения данных по свече
 type Candle struct {
-	startTime  time.Time
-	openPrice  float64
-	highPrice  float64
-	lowPrice   float64
-	closePrice float64
-	volume     float64
+	StartTime  time.Time `json:"start_time"`
+	OpenPrice  float64   `json:"open"`
+	HighPrice  float64   `json:"high"`
+	LowPrice   float64   `json:"low"`
+	ClosePrice float64   `json:"close"`
+	Volume     float64   `json:"volume"`
 }
 
 func NewCandle(startTime time.Time, open, high, low, close, volume float64) Candle {
 	return Candle{
-		startTime:  startTime,
-		openPrice:  open,
-		highPrice:  high,
-		lowPrice:   low,
-		closePrice: close,
-		volume:     volume,
+		StartTime:  startTime,
+		OpenPrice:  open,
+		HighPrice:  high,
+		LowPrice:   low,
+		ClosePrice: close,
+		Volume:     volume,
 	}
 }
 
 func (c Candle) GetStartTime() time.Time {
-	return c.startTime
+	return c.StartTime
 }
 
 func (c Candle) GetOpenPrice() float64 {
-	return c.openPrice
+	return c.OpenPrice
 }
 
 func (c Candle) GetHighPrice() float64 {
-	return c.highPrice
+	return c.HighPrice
 }
 
 func (c Candle) GetLowPrice() float64 {
-	return c.lowPrice
+	return c.LowPrice
 }
 
 func (c Candle) GetClosePrice() float64 {
-	return c.closePrice
+	return c.ClosePrice
 }
 
 func (c Candle) GetVolume() float64 {
-	return c.volume
+	return c.Volume
 }
